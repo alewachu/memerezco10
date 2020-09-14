9fa8311d29f0c9c39bb66d7aff87d7e1d37383cb
Ruben Ariel Villalobos <ariel.villalobos96@gmail.com>
2020-09-14 17:47:46 -0300

    fix renderizado primer comentario de un meme

--- frontend/src/helpers/meme.js
+++ frontend/src/helpers/meme.js
@@ -5,13 +5,16 @@ export const commentMeme = async (meme, message) => {
     comment: message,
   };
   const { data: newComment } = await post(`/api/v1/comments`, entryComment);
-  const memeWithCommentsUpdate = {
+ if(newComment){
+  const updatedMeme = await getMemeApi(meme.id);
+  return updatedMeme;
+ }
+  /*const memeWithCommentsUpdate = {
     ...meme,
     allComments: [...meme.allComments, newComment],
     comments: meme.comments + 1,
-  };
+  };*/
 
-  return memeWithCommentsUpdate;
 };
 
 export const commentParent = async (meme, message, parent) => {
@@ -33,8 +36,3 @@ export const getMemeApi = async (memeId) => {
   return response.data;
 };
 
-/*function searchCommentFather(meme,id){
- const commentFather= meme.allComments.find(c=>c.id==id);
- return commentFather;
-
-}*/
